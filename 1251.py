111a83537bc2383848c7a6dd4616160b3eae426b
redreoublics <redrepublics@yandex.ru>
2022-09-15 16:28:50 +0300

    Merge remote-tracking branch 'origin/master'

--- 1251.py
+++ 1251.py
@@ -3,6 +3,9 @@ b = input("Введите вторую строку: ")
 
 a_set, b_set = set(a), set(b) # используем множественное присваивание
 
-a_and_b = a_set.intersection(b_set)
+a_and_b = a_set.symmetric_difference(b_set)
 
 print(a_and_b)
+
+#Введите целые числа: 1 2 3 4 5 6 7 8
+#Введите целые числа: 2 4 6 8 10 12
---
+++ 131.py
@@ -0,0 +1,15 @@
+# a = int(input("Ввведите первую цифру"))
+# b = int(input("Ввведите вторую цифру"))
+# if a>b:
+#     print ("Первое значение больше второго")
+# elif a<b:
+#     print("Первое значение меньше второго")
+# else:
+#     print("Значения равны")
+
+print(True and not False)
+ # True
+print(True and False)
+# False
+
+print(not False and True or False and not True)
