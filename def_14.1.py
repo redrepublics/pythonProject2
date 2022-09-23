c374c7a624ad9134211271d292d46fd40b91a4e3
redreoublics <redrepublics@yandex.ru>
2022-09-23 12:15:20 +0300

    Merge remote-tracking branch 'origin/master'

--- def_14.1.py
+++ def_14.1.py
@@ -1,20 +1,29 @@
-def lacl_test(base):
-    if base % 2 == 0:
-        print("Четное число")
+# def lacl_test(base):
+#     if base % 2 == 0:
+#         print("Четное число")
+#     else:
+#         print("Нечетное число")
+# while True:
+#     try:
+#         lacl_test(int(input("Введите число, для определения, четное оно или нет:\n")))
+#     except ValueError as ve:
+#         print("Ошибочный параметр. Нужно ввести только целые числа.")
+#         print("Попробуем снова.")
+#         continue
+#     else:
+#         break
+
+def fin_coount (a,b):
+    if a>b:
+        count = a-b
+    elif a<b:
+        count = a+b
     else:
-        print("Нечетное число")
-while True:
-    try:
-        lacl_test(int(input("Введите число, для определения, четное оно или нет:\n")))
-    except ValueError as ve:
-        print("Ошибочный параметр. Нужно ввести только целые числа.")
-        print("Попробуем снова.")
-        continue
-    else:
-        break
+        count = 0
 
 
-
-
-
-
+    return count
+n1 = int(input())
+n2 = int(input())
+def_num = fin_coount(n1,n2)
+print(def_num)
