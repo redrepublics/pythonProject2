366bbbb2ebb85064f42cd3f91ade0d08bf10d45b
redreoublics <redrepublics@yandex.ru>
2022-09-23 12:15:20 +0300

    Merge remote-tracking branch 'origin/master' #функции

--- def_14.1.py
+++ def_14.1.py
@@ -1,4 +1,20 @@
-def sum_fun():
-    num_fun = 2+2
-    print(num_fun)
-sum_fun()
+def lacl_test(base):
+    if base % 2 == 0:
+        print("Четное число")
+    else:
+        print("Нечетное число")
+while True:
+    try:
+        lacl_test(int(input("Введите число, для определения, четное оно или нет:\n")))
+    except ValueError as ve:
+        print("Ошибочный параметр. Нужно ввести только целые числа.")
+        print("Попробуем снова.")
+        continue
+    else:
+        break
+
+
+
+
+
+
