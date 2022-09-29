540a44ac266df3743087099b4e91183bc26bee95
redreoublics <redrepublics@yandex.ru>
2022-09-29 15:11:10 +0300

    merge origin/master --no-stat -v #почти готовый консольный калькулятор

--- game_number.py
+++ game_number.py
@@ -5,11 +5,12 @@ from random import randint
 num_random = randint(1,10)
 loading = 'LOADING...'
 for i in range(10):
-    print(loading[i], sep=' ', end=' ', flush=True); sleep(0.5)
+    print(loading[i], sep=' ', end=' ', flush=True);
+    sleep(0.5)
 
 print("Game 'Escape from the mountain troll' is loaded.\n")
 print('Давайте поиграем в игру "Убеги от горного тролля".')
-print('На пути у вас дверь с волшебным замком.')
+print('На пути у Вас дверь с волшебным замком. Замок заколдован.')
 print('Gamemaster подскзаывает: Назовите волбешное число от 1 до 10 и тогда вы спасетесь!')
 print('У вас всего 3 попытки, иначе тролль догонит вас и съест.')
 
@@ -23,18 +24,24 @@ while True:
         print("Вы напрасно потратили попытку.")#, except_print
         print(f"Попытка №{count_c}")
         continue
-    if num_user != num_random and count_c < 3:
+    if num_user >10 or num_user < 1:
         count_c += 1
+        print("Посмотрите подсказку гейммастера. Вы напрасно потратили попытку. ")  # , except_print
+        print(f"Попытка №{count_c}")
+        continue
+    elif num_user != num_random and count_c < 3:
+        count_c += 1
+        print("Не угадали.")
         print(f"Попытка №{count_c}")
         continue
     elif count_c == 3:
-        print(f"Вы проиграли. Тролль догнал вас и ударил по голове кулаком. Волшебная цифра была {num_random}")
+        print(f"Вы проиграли. Тролль догнал вас и ударил по голове кулаком. Волшебная цифра была {num_random}.")
         break
     elif num_user == num_random:
         print("Вы угадали! Ура! Дверь распахнулась и вы убежали от тролля.")
         if count_c == 1:
             print("Ваше звание БОЛЬШОЙ МОЛОДЕЦ! Вы угадали с первой попытки.")
-        elif count_c ==2:
+        elif count_c == 2:
             print("Ваше звание МОЛОДЕЦ! Вы угадали со второй попытки.")
         else:
             print("Вы угадали с третьей попытки.")
