edd03e816f6caca543de02da61c6b97c3422b7b9
redreoublics <redrepublics@yandex.ru>
2022-09-29 14:03:23 +0300

    merge origin/master --no-stat -v #почти готовый консольный калькулятор

---
+++ game_number.py
@@ -0,0 +1,46 @@
+import random
+import time
+from time import sleep
+from random import randint
+num_random = randint(1,10)
+loading = 'LOADING...'
+for i in range(10):
+    print(loading[i], sep=' ', end=' ', flush=True); sleep(0.5)
+
+print("Game 'Escape from the mountain troll' is loaded.\n")
+print('Давайте поиграем в игру "Убеги от горного тролля".')
+print('На пути у вас дверь с волшебным замком.')
+print('Gamemaster подскзаывает: Назовите волбешное число от 1 до 10 и тогда вы спасетесь!')
+print('У вас всего 3 попытки, иначе тролль догонит вас и съест.')
+
+count_c = 1
+
+while True:
+    try:
+        num_user = int(input("Введите волшебное число: "))
+    except ValueError as except_print:
+        count_c += 1
+        print("Вы напрасно потратили попытку.")#, except_print
+        print(f"Попытка №{count_c}")
+        continue
+    if num_user != num_random and count_c < 3:
+        count_c += 1
+        print(f"Попытка №{count_c}")
+        continue
+    elif count_c == 3:
+        print(f"Вы проиграли. Тролль догнал вас и ударил по голове кулаком. Волшебная цифра была {num_random}")
+        break
+    elif num_user == num_random:
+        print("Вы угадали! Ура! Дверь распахнулась и вы убежали от тролля.")
+        if count_c == 1:
+            print("Ваше звание БОЛЬШОЙ МОЛОДЕЦ! Вы угадали с первой попытки.")
+        elif count_c ==2:
+            print("Ваше звание МОЛОДЕЦ! Вы угадали со второй попытки.")
+        else:
+            print("Вы угадали с третьей попытки.")
+        break
+    else:
+        print("Что-то пошло не так.")
+
+# print (num_random)
+
