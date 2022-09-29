ecd589af5f699481647e5cd4424163e993bcdaaa
redreoublics <redrepublics@yandex.ru>
2022-09-29 12:41:19 +0300

    merge origin/master --no-stat -v #почти готовый консольный калькулятор

--- dict.py
+++ dict.py
@@ -1,9 +1,13 @@
-import keyboard
-while True:
-    if keyboard.wait("esc"):
-        print("esc")
-        break
-    else:
-        print("!")
-        break
-
+# import keyboard
+# a = int(input("Введите значение:\n"))
+# while True:
+#     if a >= 10:
+#         print("Нажмите ESC")
+#         n = keyboard.wait('esc') or keyboard.wait('1')
+#         if n == 'esc':
+#             print("А больше или равна 10")
+#             continue
+#         elif n == '1':
+#             print("А меньше 10")
+#             break
+#
