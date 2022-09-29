f3d2173aa1f1f1517a9439cb7a42a884299a9175
redreoublics <redrepublics@yandex.ru>
2022-09-29 16:18:32 +0300

    merge origin/master --no-stat -v #почти готовый консольный калькулятор

--- game_number.py
+++ game_number.py
@@ -3,17 +3,17 @@ import time
 from time import sleep
 from random import randint
 num_random = randint(1,10)
-loading = 'LOADING...'
-for i in range(10):
-    print(loading[i], sep=' ', end=' ', flush=True);
-    sleep(0.5)
+# loading = 'LOADING...'
+# for i in range(10):
+#     print(loading[i], sep=' ', end=' ', flush=True);
+#     sleep(0.5)
 
 print("Game 'Escape from the mountain troll' is loaded.\n")
 print('Давайте поиграем в игру "Убеги от горного тролля".')
 print('На пути у Вас дверь с волшебным замком. Замок заколдован.')
 print('Gamemaster подскзаывает: Назовите волбешное число от 1 до 10 и тогда вы спасетесь!')
 print('У вас всего 3 попытки, иначе тролль догонит вас и съест.')
-#print (num_random) #для тестов
+print (num_random) #для тестов
 count_c = 1
 
 while True:
@@ -24,7 +24,7 @@ while True:
         print("Вы напрасно потратили попытку.")#, except_print
         print(f"Попытка №{count_c}")
         continue
-    if num_user > 10 or num_user < 1:
+    if (num_user > 10 or num_user < 1) and count_c != 3:
         count_c += 1
         print("Посмотрите подсказку Gamemaster-а. Вы напрасно потратили попытку. ")  # , except_print
         print(f"Попытка №{count_c}")
