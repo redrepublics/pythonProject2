0125c1a6c17c5c7924f94a7281d707f51f94a38d
redreoublics <redrepublics@yandex.ru>
2022-09-29 16:26:42 +0300

    merge origin/master --no-stat -v #почти готовый консольный калькулятор

--- game_number.py
+++ game_number.py
@@ -1,27 +1,21 @@
+#задаем переменные и импортируем библиотеки
 import random
-import time
-from time import sleep
 from random import randint
 num_random = randint(1,10)
-# loading = 'LOADING...'
-# for i in range(10):
-#     print(loading[i], sep=' ', end=' ', flush=True);
-#     sleep(0.5)
-
-print("Game 'Escape from the mountain troll' is loaded.\n")
+count_c = 1
+#начало интерактива
 print('Давайте поиграем в игру "Убеги от горного тролля".')
 print('На пути у Вас дверь с волшебным замком. Замок заколдован.')
 print('Gamemaster подскзаывает: Назовите волбешное число от 1 до 10 и тогда вы спасетесь!')
 print('У вас всего 3 попытки, иначе тролль догонит вас и съест.')
-print (num_random) #для тестов
-count_c = 1
+#print (num_random) #для тестов
 
+#цикл с основными условиями
 while True:
     try:
         num_user = int(input("Введите волшебное число: "))
     except ValueError as except_print:
-        count_c += 1
-        print("Вы напрасно потратили попытку.")#, except_print
+        print("В игре можно использовать только целые числа! Попытка не засчитана.")#, except_print
         print(f"Попытка №{count_c}")
         continue
     if (num_user > 10 or num_user < 1) and count_c != 3:
