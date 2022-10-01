fa3eaf91e2d6c8aabef7e209c6f09decaa77c10c
redreoublics <redrepublics@yandex.ru>
2022-10-01 14:15:07 +0300

    merge origin/master #доработка игры, добавление переменных

--- game_number.py
+++ game_number.py
@@ -8,12 +8,12 @@ num_ranodom_min, num_ranodom_max = 1, 10
 count_c, count_c_max, =  num_ranodom_min, 3
 num_random = randint(num_ranodom_min, num_ranodom_max)
 num_random_ave = num_ranodom_max / 2
-
+username_game = input("Как вас зовут?")
 
 #начало интерактива
-print(f"""\033[34mДавайте поиграем в игру 'Убеги от горного тролля'.
+print(f"""\033[34mДавайте поиграем в игру,'Убеги от горного тролля'.
 На пути у Вас дверь с волшебным замком. Замок заколдован.
-Gamemaster подскзаывает: Назовите волшебное число от {num_ranodom_min} до {num_ranodom_max} и тогда вы спасетесь!
+Gamemaster подскзаывает: {username_game}, назовите волшебное число от {num_ranodom_min} до {num_ranodom_max} и тогда вы спасетесь!
 У вас всего {count_c_max} попытки, иначе тролль догонит вас и съест.\033[0m""")
 #print (num_random) #для тестов
 
@@ -26,11 +26,11 @@ while True:
         continue
     if (num_user not in range(num_ranodom_min, num_ranodom_max+1)) and count_c != count_c_max:
         count_c += 1
-        print(f"Ваше волшебное число не подходит к условиям игры!\nЧисла доллжны быть от {num_ranodom_min} до {num_ranodom_max}.\nПопытка №{count_c}")
+        print(f"Ваше волшебное число не подходит к условиям игры!\nЧисла, доллжны быть, от {num_ranodom_min} до {num_ranodom_max}.\nПопытка №{count_c}")
         continue
     elif num_user != num_random and count_c < count_c_max:
         count_c += 1
-        print(f"Не угадали.\nПопытка №{count_c}.")
+        print(f"Увы,{username_game},но вы не угадали.\nПопытка №{count_c}.")
         if count_c == count_c_max and num_random <= num_random_ave:  # меньше или равно
             print("\033[34mGamemaster: Число может быть в первой части последовательности.\033[0m")
             continue
@@ -39,7 +39,7 @@ while True:
             continue
         continue
     elif num_user == num_random:
-        print("Вы угадали! Ура! Дверь распахнулась и вы убежали от тролля.")
+        print(f"\033[32m{username_game}, вы угадали! Ура! Дверь распахнулась и вы убежали от тролля.\033[0m")
         if count_c == num_ranodom_min:
             print(f"Ваше звание БОЛЬШОЙ МОЛОДЕЦ! Вы угадали с {count_c} попытки.")
         elif count_c == 2:
@@ -48,7 +48,7 @@ while True:
             print(f"Вы угадали с {count_c} попытки.")
         break
     elif count_c == count_c_max:
-        print(f"Вы проиграли. Тролль догнал вас и ударил по голове кулаком. Волшебная цифра была {num_random}.")
+        print(f"Вы проиграли {username_game}. Тролль догнал вас и ударил по голове кулаком. Волшебная цифра была {num_random}.")
         break
     else:
         print("Что-то пошло не так.")
