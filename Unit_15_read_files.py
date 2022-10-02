66f3a347c083ba2705cadb78334d329f1095c794
redreoublics <redrepublics@yandex.ru>
2022-10-02 13:21:31 +0300

    merge origin/master #готовый вариант игры с персонализацией игрока и всеми условиями

--- Unit_15_read_files.py
+++ Unit_15_read_files.py
@@ -1,13 +1,16 @@
+#код нацелен на папку запуска скрипта
+#если папки log нет, она там создается. Если есть, действие по всей схеме не происходит. Проработать этот момент.
+# может стоит отказаться от папки (излишнее)
 import os
+import glob
 import time
 from datetime import datetime
 from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%y_%m_%d_%H_%M_%S")
-
-
-with open('python_read_file.txt', 'rt', encoding="utf-8") as file:
-# with open(os.path.join('E:', 'python_read_file.txt'), 'rt', encoding="utf-8") as file:
+# for filename in glob.glob('*.txt'):
+#     with open(os.path.join(os.getcwd(), filename), 'rt', encoding="utf-8") as file:
+with open(os.path.join('E:', 'python_read_file.txt'), 'rt', encoding="utf-8") as file:
     error_per = file.readlines()
     # print(error_per)
     error_search = 'Error'
@@ -19,8 +22,8 @@ with open('python_read_file.txt', 'rt', encoding="utf-8") as file:
         print('Ошибки обнаружены и записаны.')
         my_result.close()
         file.close()
-        old_file = os.path.join("E:", "log_test_final.txt")
-        new_file = os.path.join("E:", f"aspo_error{current_time}.txt")
+        old_file = os.path.join("E:", "logs", "log_test_final.txt")
+        new_file = os.path.join("E:", "logs", f"aspo_error{current_time}.txt")
         os.rename(old_file, new_file)
     else:
         print('Ошибок нет')
