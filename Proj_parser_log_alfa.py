e92ce99d6892e8b71840ce4a0f473123c67572ab
redreoublics <redrepublics@yandex.ru>
2022-10-02 14:29:13 +0300

    merge origin/master #ссмотрим каменты к коду в скрипте

--- Proj_parser_log_alfa.py
+++ Proj_parser_log_alfa.py
@@ -2,19 +2,28 @@
 #посмотреть в py to exe, можно ли принтануть юзергайд
 #если папки log нет, она там создается. Если есть, действие по всей схеме не происходит. Проработать этот момент.
 
+#тут импортируем все модули
 import os
 import glob
 import time
 from datetime import datetime
-from datetime import datetime
+
+#задаем уникальную переменную для файла вывода
 now = datetime.now()
 current_time = now.strftime("%y_%m_%d_%H_%M_%S")
 
 # with open(os.path.join('E:', 'test.txt'), 'rt', encoding="utf-8") as file:
-with open(os.path.join('E:', 'test.txt'), 'rt',) as file:
-    error_per = file.readlines()
-    # print(error_per)
-    error_search = 'Error' or 'error'
+
+#обрабатываем ошибку отсутствия нужного файла в нужной папке, иначе записываем файл, переименовываем его с временной
+#меткой указанной выше, не забыв закрыть рабочие файлы, дабы не допустить утечку памяти при работе
+try:
+    with open(os.path.join('E:', 'test.txt'), 'rt',) as file:
+        error_per = file.readlines()
+except FileNotFoundError:
+    print(f"Запрашиваемый файл не найден")
+# print(error_per)
+else:
+    error_search = 'Error' or 'error' or 'Exception'
     final = "\n".join(s for s in error_per if error_search.lower() in s.lower())
     if final:
         my_result = open(os.path.join('E:', 'log_test_final.txt'), 'w', encoding="utf-8")
