e26d431c6f633ab6b81e884aecbe258797b52014
redreoublics <redrepublics@yandex.ru>
2022-10-03 11:08:52 +0300

    merge origin/master #ссмотрим каменты к коду в скрипте

--- rename.py
+++ rename.py
@@ -2,14 +2,14 @@
 import os
 import sys
 import glob
-folder = 'E:'
-for filename in os.listdir(folder):
-       infilename = os.path.join(folder,filename)
-       if not os.path.isfile(infilename):
-           continue
-       oldbase = os.path.splitext(filename)
-       newname = infilename.replace('.xml', '.txt')
-       output = os.rename(infilename, newname)
+folder = os.getcwd()
+def folder_dir():
+    for filename in os.listdir(folder):
+        infilename = os.path.join(folder,filename)
+        if not os.path.isfile(infilename):
+            continue
+        else:
+            oldbase = os.path.splitext(filename)
+            newname = infilename.replace('.xml', '.txt')
+            output = os.rename(infilename, newname)
 
-for f in glob.glob("E:\*.txt"):
-    os.system("cat " + f + " >> OutFile.txt")
