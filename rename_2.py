18ad114c8bae423d625f0cb6b794456fc73fa29c
redreoublics <redrepublics@yandex.ru>
2022-10-03 12:03:48 +0300

    merge origin/master #ссмотрим каменты к коду в скрипте

--- rename_2.py
+++ rename_2.py
@@ -1,11 +1,27 @@
-import pandas as pd
-import glob
-import os
+import glob, os
 folder = os.getcwd()
-files = glob.glob('*.txt')
-combined = pd.DataFrame()
+# каталог текстовых файлов
+# измените на свой
+path = folder
+# паттерн поиска файлов по расширению
+pattern = '*.txt'
 
-for file in files:
-    data = pd.read_txt(file)
-    data['result_filename'] = file
-    combined = pd.concat([combined, data])
+glob_path = os.path.join(path, pattern)
+list_files = glob.glob(glob_path)
+# расширение нового файла установим как '.all'
+new_file = 'new_file.all'
+
+# чтение и запись
+if list_files:
+    for file_name in list_files:
+        # открываем файл из 'list_files' на чтение
+        # а новый общий файл 'new_file' на дозапись
+        with open(file_name, 'r') as fr, open(new_file, 'a') as fw:
+            # дописываем строку с названием файла
+            fw.write(f'\n\n------------ {file_name}\n\n')
+
+            # читаем данные построчно
+            for line in fr:
+                # если нужно, то здесь обрабатываем каждую строку 'line'
+                # после обработки дописываем в общий файл
+                fw.write(line)
