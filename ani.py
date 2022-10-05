3b39f2c47824fa91f1ae79fc4f324cb6a8c881cb
redreoublics <redrepublics@yandex.ru>
2022-10-05 16:39:55 +0300

    merge origin/master #комит парсера с финальынми правками, только для win10 и выше

--- ani.py
+++ ani.py
@@ -13,10 +13,28 @@ def connect_sql():
     dbCursor = connection.cursor()
     requestString = ('select top(10)*, LName from tUserDetails order by RecTime DESC')
     requestString2 = ('select @@VERSION')
-    dbCursor.execute(requestString)
-    connection.commit()
-    for respond_sql in dbCursor:
-        print(respond_sql)
+
+    while True:
+        global request_user
+        try:
+            request_user = int(input("Запрос в tUserDetails............[1]\nУзнать версию сервера MS SQL.....[2]\n"))
+        except ValueError or NameError:
+            print('Я так не умею')
+            break
+        if request_user == 1:
+            dbCursor.execute(requestString)
+            connection.commit()
+            for respond_sql in dbCursor:
+                print(respond_sql)
+            break
+        elif request_user == 2:
+            dbCursor.execute(requestString2)
+            connection.commit()
+            for respond_sql in dbCursor:
+                print(respond_sql)
+            break
+        else:
+            print('Что-то пошло не так.')
 
 connect_sql()
 
