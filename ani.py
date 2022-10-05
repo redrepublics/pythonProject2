1ad89637f5d24ce7cb7be51eb8d6512aa931bf88
redreoublics <redrepublics@yandex.ru>
2022-10-05 13:37:58 +0300

    merge origin/master #комит парсера с финальынми правками, только для win10 и выше

--- ani.py
+++ ani.py
@@ -1,18 +1,16 @@
-import itertools, sys
-
-
-
-
-
-def anim_def():
-    spinner = itertools.cycle(['-', '/', '|', '\\'])
-    while True:
-        sys.stdout.write(next(spinner))   # write the next character
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
-        sys.stdout.write('\b')            # erase the last written char
-        continue
-
-
-anim_def()
-
+# import pyodbc
+#
+# driver_id_list = pyodbc.drivers()
+# driver_id = driver_id_list[3]
+# print(driver_id_list)
+# print(driver_id)
+from pymssql import connect
+server = r'ZVERDVD-OKOC0U9\SQLEXPRESS' # I've also tried MORGANT-PC\SQLEXPRESS and SQLEXPRESS2014
+username = '.\sa'
+password = 'daorliar'
+master_database_name = 'srw_688'
+port = 1433
+server_args = {'host': server, 'user': username, 'password': password,
+               'database': master_database_name, 'port': port} # I've tried having the first key be both host and server, because pymssql's docs are unclear on the difference.
+master_database = connect(**server_args)
 
