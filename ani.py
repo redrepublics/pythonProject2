b83776e636d2fc0737bd1262bd2b4765742b79ee
redreoublics <redrepublics@yandex.ru>
2022-10-05 16:14:27 +0300

    merge origin/master #комит парсера с финальынми правками, только для win10 и выше

--- ani.py
+++ ani.py
@@ -7,17 +7,18 @@ drever_id_sql = driver_id[1]
 server= 'ZVERDVD-OKOC0U9\SQLEXPRESS'
 database = 'srw_688'
 
+def connect_sql():
+    connectionString = ("Driver={"+drever_id_sql+"};" "Server="+server+";" "Database="+database+";" "Trusted_Connection=yes;")
+    connection = pyodbc.connect(connectionString, autocommit=True)
+    dbCursor = connection.cursor()
+    requestString = ('select top(10)*, LName from tUserDetails order by RecTime DESC')
+    requestString2 = ('select @@VERSION')
+    dbCursor.execute(requestString)
+    connection.commit()
+    for respond_sql in dbCursor:
+        print(respond_sql)
 
-connectionString = ("Driver={"+drever_id_sql+"};" "Server="+server+";" "Database="+database+";" "Trusted_Connection=yes;")
-connection = pyodbc.connect(connectionString, autocommit=True)
-dbCursor = connection.cursor()
-requestString = ('select top(10)*, LName from tUserDetails order by RecTime DESC')
-dbCursor.execute(requestString)
-connection.commit()
-for respond_sql in dbCursor:
-    print(respond_sql)
-
-
+connect_sql()
 
 
 
