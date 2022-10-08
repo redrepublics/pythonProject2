333b8e4705d4b9e841681fe3f042da704f0d077f
redreoublics <redrepublics@yandex.ru>
2022-10-08 13:05:15 +0300

    merge origin/master #классы

--- unit16_3_class.py
+++ unit16_3_class.py
@@ -2,25 +2,36 @@
 class Employee:
     """Базовый класс для всех сотрудников"""
     emp_count = 0
+    global dict1, dict2, dict3
+    dict1, dict2, dict3 = 'IT', 'Бухгалтерия', 'Охрана'
 
-    def __init__(self, name, salary):
+
+    def __init__(self, name, salary, dist, age):
         self.name = name
         self.salary = salary
+        self.dist = dist
+        self.age = age
         Employee.emp_count += 1
 
     def display_count(self):
         print('Всего сотрудников: %d' % Employee.emp_count)
 
     def display_employee(self):
-        print('Имя: {}. Зарплата: {}'.format(self.name, self.salary))
+        print('Имя: {}. Зарплата: {}. Отдел: {}. Возраст: {}.'.format(self.name, self.salary, self.dist, self.age))
+
+
+
 
     # Это создаст первый объект класса Employee
 
 
-emp1 = Employee("Андрей", 2000)
+
+
+
+emp1 = Employee("Андрей", 2000, dict1, 20)
 # Это создаст второй объект класса Employee
-emp2 = Employee("Мария", 5000)
-emp3 = Employee("Денис", 2000)
+emp2 = Employee("Мария", 5000, dict2, 35)
+emp3 = Employee("Денис", 2000, dict3, 40)
 emp1.display_employee()
 emp2.display_employee()
 emp3.display_employee()
