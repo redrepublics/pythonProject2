e4bb0ff435f7e7ac5741b7aac51873a84f4cbf1e
redreoublics <redrepublics@yandex.ru>
2022-10-13 23:29:34 +0300

    merge origin/master #классы в утилите, все короткие запросы в них, на выходе утилита обновления АСПО

---
+++ Parser_log_aspo/GetParser.py
@@ -0,0 +1,39 @@
+import os
+folder = os.getcwd()
+init_list = ['.xml', '.txt', 'test.xml', 'pars_result']
+format_start = init_list[0]
+format_finish = init_list[1]
+test_xml = init_list[2]
+my_dir = init_list[3]
+
+def dir_cr():
+    check_folder = os.path.isdir(my_dir)
+    if not check_folder:
+        os.makedirs(my_dir)
+        return print("Создана папка : ", my_dir)
+
+
+#переводим xml в txt
+def folder_dir():
+    for filename in os.listdir(folder):
+        infilename = os.path.join(folder, filename)
+        if not os.path.isfile(infilename):
+            continue
+        else:
+            oldbase = os.path.splitext(filename)
+            newname = infilename.replace(format_start, format_finish)
+            output = os.rename(infilename, newname)
+
+
+#переводим txt в xml
+def folder_dir_return():
+    for filename in os.listdir(folder):
+        infilename = os.path.join(folder, filename)
+        if not os.path.isfile(infilename):
+            continue
+        else:
+            oldbase = os.path.splitext(filename)
+            newname = infilename.replace(format_finish, format_start)
+            output = os.rename(infilename, newname)
+
+
--- Parser_log_aspo/parser_logs_aspo.py
+++ Parser_log_aspo/parser_logs_aspo.py
@@ -2,60 +2,17 @@
 import os, glob, time, shutil
 from datetime import datetime
 from parserDef import GetIni
-
+from GetParser import dir_cr, folder_dir, folder_dir_return
+ver = '1.0.0.7 alfa'
 #блок глобальных переменных
 now = datetime.now()
 current_time = now.strftime("%y_%m_%d_%H_%M_%S")
 folder = os.getcwd()
-ver = '1.0.0.7 alfa'
-format_start = '.xml'
-format_finish = '.txt'
-format_rtf = '.rtf'
-name_data_file = 'test.txt'
-log_test_final = 'log_test_final.txt'
-test_xml = 'test.xml'
-my_dir = 'pars_result'
-
-#поиск и создание результирующей папки
-def dir_cr():
-    check_folder = os.path.isdir(my_dir)
-    if not check_folder:
-        os.makedirs(my_dir)
-        print("Создана папка : ", my_dir)
-    else:
-        print("Папка", my_dir, "уже существует.")
-
-#переводим xml в txt
-def folder_dir():
-    for filename in os.listdir(folder):
-        infilename = os.path.join(folder, filename)
-        if not os.path.isfile(infilename):
-            continue
-        else:
-            oldbase = os.path.splitext(filename)
-            newname = infilename.replace(format_start, format_finish)
-            output = os.rename(infilename, newname)
-
-#переводим txt в xml
-def folder_dir_return():
-    for filename in os.listdir(folder):
-        infilename = os.path.join(folder, filename)
-        if not os.path.isfile(infilename):
-            continue
-        else:
-            oldbase = os.path.splitext(filename)
-            newname = infilename.replace(format_finish, format_start)
-            output = os.rename(infilename, newname)
-
-def folder_dir_return_rtf():
-    for filename in os.listdir(folder):
-        infilename = os.path.join(folder, filename)
-        if not os.path.isfile(infilename):
-            continue
-        else:
-            oldbase = os.path.splitext(filename)
-            newname = infilename.replace(format_rtf, format_finish)
-            output = os.rename(infilename, newname)
+init_list = ['test.txt', 'log_test_final.txt', 'test.xml', 'pars_result', '*.txt']
+name_data_file = init_list[0]
+log_test_final = init_list[1]
+test_xml = init_list[2]
+my_dir = init_list[3]
 
 #формирование результирующего файла
 def files_sum():
@@ -63,12 +20,12 @@ def files_sum():
     # измените на свой
     path = folder
     # паттерн поиска файлов по расширению
-    pattern = '*.txt'
+    pattern = init_list[4]
 
     glob_path = os.path.join(path, pattern)
     list_files = glob.glob(glob_path)
     # расширение нового файла установим как '.txt'
-    new_file = 'test.txt'
+    new_file = name_data_file
 
     # чтение и запись
     if list_files:
@@ -77,8 +34,7 @@ def files_sum():
             # а новый общий файл 'new_file' на дозапись
             with open(file_name, 'r') as fr, open(new_file, 'a') as fw:
                 # дописываем строку с названием файла
-                fw.write(f'\n\n------------ {file_name}\n\n')
-
+                fw.write(f'\n\n---Запись найдена в файле {file_name}\n\n')
                 # читаем данные построчно
                 for line in fr:
                     # если нужно, то здесь обрабатываем каждую строку 'line'
@@ -113,11 +69,20 @@ else:
     final_two = "\n".join(s for s in error_per if error_search_two.lower() in s.lower())
     final_three = "\n".join(s for s in error_per if error_search_three.lower() in s.lower())
 
-    if final_one or final_two:# or final_three:
+    if final_one or final_two or final_three:
         my_result = open(os.path.join(folder, log_test_final), 'w+')
-        my_result.write(r''+final_one+'\n')
-        my_result.write(r''+final_two+'\n')
-        my_result.write(r''+final_three+'\n')
+        if final_one:
+            my_result.write(r''+final_one+'\n')
+        else:
+            pass
+        if final_two:
+            my_result.write(r''+final_two+'\n')
+        else:
+            pass
+        if final_three:
+            my_result.write(r''+final_three+'\n')
+        else:
+            pass
         my_result.close()
         file.close()
         old_file = os.path.join(folder,  log_test_final)
@@ -130,6 +95,7 @@ else:
         time.sleep(5)
     else:
         os.remove(os.path.join(folder, name_data_file))
+        folder_dir_return()
         print('Ошибок не обнаружено.')
         time.sleep(5)
 # окончание блока парсера
