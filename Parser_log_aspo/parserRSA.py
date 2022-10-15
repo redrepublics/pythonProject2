1dc7b0c86c9fb222661b14a995ba5a93e6ef9eeb
redreoublics <redrepublics@yandex.ru>
2022-10-16 00:41:55 +0300

    merge origin/master #классы в утилите, все короткие запросы в них, на выходе утилита обновления АСПО

--- Parser_log_aspo/parserRSA.py
+++ Parser_log_aspo/parserRSA.py
@@ -1,6 +1,4 @@
 import rsa, os
-from GetParser import dir_cr, folder_dir, folder_dir_return
-folder = os.getcwd()
 # RSA - криптографический алгоритм с открытым ключем. При создании приложения вы
 # генерируете два ключа: публичный (открытый) и приватный (закрытый).
 # Открытый ключ передается всем желающим и заинтерисованным.
@@ -8,41 +6,33 @@ folder = os.getcwd()
 # А вот расшифровать можно только знаю другой ключ из пары (т.е. закрытый),
 # его мы никому не скажем даже под страхом смерти
 # (pubkey, privkey) = rsa.newkeys(512) - сгенерить новые ключи по необходимости
-
+folder = os.getcwd()
 KeyParserAspo = b'17110367'
 key_lust = []
 key_lust.append(rsa.PublicKey(8611358230186391020299884467627008805631867422184296025775040113155292187404091008391828212109012111160193892945618249623785973404528075573378180488699411, 65537))
 key_lust.append(rsa.PrivateKey(8611358230186391020299884467627008805631867422184296025775040113155292187404091008391828212109012111160193892945618249623785973404528075573378180488699411, 65537, 1785289596312655367087515911037248709005907848470604850728679524809511495531410086323245025094710056967296658966006870838551894074625906357076875122535305, 4976621894468449988039155076339535136070221736664167314792320432105946835123890807, 1730362163892333445841066415765865910608387667078479960761628212597781573))
-pubkey = key_lust[0]
-privkey = key_lust[1]
-if KeyParserAspo:
+pubkey, privkey = key_lust[0], key_lust[1]
+
+def CriptoRSA():
+    if KeyParserAspo:
+    # блок создания нового файла по переменной KeyParserAspo
     # crypto = rsa.encrypt(KeyParserAspo, pubkey)
+    #     with open(os.path.join(folder, 'validaspo.bin'), 'wb+') as result:
+    #         result.write(rsa.encrypt(KeyParserAspo, pubkey))
+    #         result.close()
+        with open(os.path.join(folder, 'validaspo.bin'), 'rb') as result:
+            content = result.read()
+            message = rsa.decrypt(content, privkey)
+            result.close()
+            if message == KeyParserAspo:
+                print("Валидация пройдена")
+                RSAReturn = True
+            else:
+                print("Провал")
+                RSAReturn = False
+    return RSAReturn
 
-
-    with open(os.path.join(folder, 'validaspo.bin'), 'rb') as result:
-        content = result.read()
-        print(content)
-
-        # message = rsa.decrypt(, privkey)
-        # if message in KeyParserAspo:
-        #     print("Валидация пройдена")
-        # else:
-        #     print("Валидация провалена")
-        # # result.write(b"crypto")
-        # result.close()
-
-    # with open(os.path.join(folder, 'validaspo.bin'), 'wb+') as result:
-    #     result.write(b"crypto")
-    #     result.close()
-    #
-
-    # message = rsa.decrypt(crypto, privkey)
-    # x, y = crypto, message
-    # lust_cr = []
-    # lust_cr.append(x)
-    # lust_cr.append(y)
-    # print(lust_cr[0])
-    # print(lust_cr[1])
+CriptoRSA()
 
 
 
