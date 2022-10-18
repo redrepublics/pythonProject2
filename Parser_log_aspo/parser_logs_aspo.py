23ab0499e17591638e8834d996b692b684506e8c
redreoublics <redrepublics@yandex.ru>
2022-10-18 17:08:37 +0300

    merge origin/master #17.9.1 (HW-03)

--- Parser_log_aspo/parser_logs_aspo.py
+++ Parser_log_aspo/parser_logs_aspo.py
@@ -72,22 +72,30 @@ else:
     final_one = "\n".join(s for s in error_per if error_search_one.lower() in s.lower())
     final_two = "\n".join(s for s in error_per if error_search_two.lower() in s.lower())
     final_three = "\n".join(s for s in error_per if error_search_three.lower() in s.lower())
-    my_result = open(os.path.join(folder, log_test_final), 'w+', encoding='utf-8')
+
     #0 все, 1 Eror , 2 Exception, 3 ini
 
     if final_one or final_two or final_three:
-        if str(DepthIni()) in '0':
-            my_result.write(r''+final_one+'\n',)
-            my_result.write(r'' + final_two + '\n')
-            my_result.write(r'' + final_three + '\n')
-        elif str(DepthIni()) in '1':
+        my_result = open(os.path.join(folder, log_test_final), 'w+', encoding='utf-8')
+        if DepthIni() == 0:
             my_result.write(r''+final_one+'\n')
-        elif str(DepthIni()) in '2':
-            my_result.write(r''+final_two+'\n')
-        elif str(DepthIni()) in '3':
+        else:
+            pass
+        if DepthIni() == 1:
+            my_result.write(r'' + final_two + '\n')
+        else:
+            pass
+        if DepthIni() == 2:
             my_result.write(r'' + final_three + '\n')
         else:
             pass
+        if DepthIni() == 3:
+            my_result.write(r'' + final_one + '\n')
+            my_result.write(r'' + final_two + '\n')
+            my_result.write(r'' + final_three + '\n')
+        else:
+            pass
+
         my_result.close()
         file.close()
         old_file = os.path.join(folder,  log_test_final)
