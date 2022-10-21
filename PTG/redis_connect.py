7f5a1402662e92e766414f452f906de592055fcd
redreoublics <redrepublics@yandex.ru>
2022-10-21 17:16:03 +0300

    merge origin/master #PEP-8

---
+++ PTG/redis_connect.py
@@ -0,0 +1,9 @@
+import redis
+
+red = redis.Redis(
+    host='redis-12223.c245.us-east-1-3.ec2.cloud.redislabs.com',
+    port=12223,
+    password='Zv4NSwnct9fgU4ICBlpUSjOQcBgI1dnI')
+
+# PS C:\Users\Zver\PycharmProjects\pythonProject3> C:\Users\Zver\AppData\Local\Programs\Python\Python310\python.exe -i
+# C:/Users/Zver/PycharmProjects/pythonProject3/PTG/redis_connect.pyred
