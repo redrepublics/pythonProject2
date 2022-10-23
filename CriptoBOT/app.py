cd26dc89e0ee26717f5d7aa933c900aa4e0f4fc4
redreoublics <redrepublics@yandex.ru>
2022-10-23 20:17:58 +0300

    merge origin/master #PEP-8

--- CriptoBOT/app.py
+++ CriptoBOT/app.py
@@ -4,11 +4,16 @@ from validbot import token_key
 import requests
 
 bot = token_key()
+max_values = 3
 keys = dict(биткоин='BTC', эфириум='ETH', доллар='USD')
 
 
+class ConvertionException(Exception):
+    pass
+
+
 @bot.message_handler(commands=['start', 'help'])
-def help(message: telebot.types.Message):
+def help_get(message: telebot.types.Message):
     text = "Для начала работы введите команды в следующем формате:\n<имя валюты>" \
            "<в какую валюту перевести>" \
            "<количество переводимой валюты>\n" \
@@ -26,12 +31,33 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
-    quote, base, amount = message.text.split(' ')
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
-    total_base = json.loads(r.content)[keys[base]]
-    text = f'Стоимость {amount} {quote} в {base} составит {total_base} {base}.'
-    bot.send_message(message.chat.id, text)
+    values_input = message.text.split(' ')
+    quote, base, amount = values_input
 
+
+    if len(values_input) != 3:
+        raise ConvertionException("Слишком много параметров.")
+
+    quote_ticker, base_ticker = keys[quote], keys[base]
+    if quote == base:
+        raise ConvertionException(f"Вы ввели одинаковые значения валюты {base}")
+    try:
+        quote_ticker = keys[quote]
+    except KeyError:
+        raise ConvertionException(f"Не удалось обработать валюту {quote}")
+    try:
+        base_ticker = keys[base]
+    except KeyError:
+        raise ConvertionException(f"Не удалось обработать валюту {base}")
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise ConvertionException(f"Не удалось обработать количество {amount}")
+
+    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
+    total_base = json.loads(r.content)[keys[base]]
+    text = f"Стоимость {amount} {quote} в {base} составит {total_base} {base}."
+    bot.send_message(message.chat.id, text)
 
 
 bot.polling()
