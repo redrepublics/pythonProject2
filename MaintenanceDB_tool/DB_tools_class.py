059fe2f3be736b8b6f9127f2b040430be76b7a68
vjerebiov <redrepublics@yandex.ru>
2022-10-25 18:54:06 +0300

    merge origin/master #PEP-8

--- MaintenanceDB_tool/DB_tools_class.py
+++ MaintenanceDB_tool/DB_tools_class.py
@@ -1,4 +1,4 @@
-import pyodbc
+
 
 
 class Request:
@@ -9,25 +9,7 @@ class Request:
         return self.requestString
 
 
-def get_connect():
-    # database = "SRW_688"
-    # server = "192.168.0.116"
-    # username = "sa"
-    # password = "daorliar"
-    driver = 'SQL Server Native Client 11.0'
-
-    # server = 'localhost\sqlexpress'  # для особых случаев
-    # server = 'myserver,port'  # для указания альтернативного порта
-
-    server = "192.168.0.116"  # Название сервера SQL, к которому будет выполнено подключение.
-    bddata = "SRW_688"  # Имя базы данных SQL
-    user = "sa"  # Имя пользователя SQL
-    password = "daorliar"  # Пароль пользователя SQL
-    connect = pyodbc.connect(
-        'DRIVER={' + driver + '};SERVER=' + server + ';DATABASE=' + bddata + ';UID=' + user + ';PWD=' + password + ';')
 
 
-    return connect
 
 
-get_connect()
