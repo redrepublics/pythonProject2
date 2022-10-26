40eac0de55003daf503fee7f5bf5b7452cb4c766
redreoublics <redrepublics@yandex.ru>
2022-10-26 15:10:09 +0300

    merge origin/master #Main_DB_tools_start

--- MaintenanceDB_tool/DB_tools_params.py
+++ MaintenanceDB_tool/DB_tools_params.py
@@ -5,6 +5,7 @@ from DB_tools_class import get_folder
 TODAY = datetime.date.today()
 Request1 = "SELECT @@version;"
 Request2 = 'select * from tLocalParams'
-Request3 = 'BACKUP DATABASE [' + get_folder()[3] + "] TO DISK = N'" + get_folder()[0] + "/" + get_folder()[3] + '-' + str(TODAY) + ".bak'"
+Request3 = 'BACKUP DATABASE [' + get_folder()[3] + "] TO DISK = N'" + get_folder()[0] + "/" + get_folder()[3] + '-' + str(TODAY) + ".bak'" # рабочий бэкап
 Request4 = 'exec SP_DBREINDEX'
-# Request5 = "DBCC SHRINKDATABASE(N'" + SRW_688+")"
+Request5 = 'DBCC SHRINKDATABASE ('+get_folder()[3]+', 10);' # рабочий шринк
+
--- MaintenanceDB_tool/MaintenanceDB_tool.py
+++ MaintenanceDB_tool/MaintenanceDB_tool.py
@@ -5,23 +5,42 @@ from DB_tools_class import sql_return
 
 
 
-def connect_sql():
-    cursor = sql_return().cursor()
-    cursor.execute(Request3)
-    time.sleep(1)
-    if cursor.nextset() is True:
-        print("Бэкап готов")
-        # cursor.execute(Request5)
-        # for row in cursor:
-        #     print(row)
-    else:
-        print("Неудача")
-    sql_return().close()
+# def bak_sql():
+#     cursor = sql_return().cursor()
+#     cursor.execute(Request3)
+#     time.sleep(1)
+#     if cursor.nextset() is True:
+#         print("Бэкап готов")
+#         # cursor.execute(Request5)
+#         # for row in cursor:
+#         #     print(row)
+#     else:
+#         print("Неудача")
+#     sql_return().close()
 
     # row = cursor.fetchone()
     # while row:
     #     print(row[0])
     #     row = cursor.fetchone()
 
+def sh_sql():
+    cursor = sql_return().cursor()
+    cursor.execute(Request5)
+    time.sleep(1)
+    for row in cursor:
+        print('row = %r' % (row,))
+    # row = cursor.fetchone()
+    #
+    # if row:
+    #     while row:
+    #         print(row[0])
+    #         row = cursor.fetchone()
+    # # if cursor.nextset() is True:
+    # #     print("Реиндекс готов")
+    #
+    # else:
+    #     print('Неудача')
+    sql_return().close()
+    cursor.close()
 
-connect_sql()
+sh_sql()
