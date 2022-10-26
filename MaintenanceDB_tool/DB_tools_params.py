800ceab0cfcdb145fa60e8cce2d2da15d8907a50
redreoublics <redrepublics@yandex.ru>
2022-10-26 12:45:35 +0300

    merge origin/master #Main_DB_tools_start

--- MaintenanceDB_tool/DB_tools_params.py
+++ MaintenanceDB_tool/DB_tools_params.py
@@ -6,3 +6,5 @@ TODAY = datetime.date.today()
 Request1 = "SELECT @@version;"
 Request2 = 'select * from tLocalParams'
 Request3 = 'BACKUP DATABASE [' + get_folder()[3] + "] TO DISK = N'" + get_folder()[0] + "/" + get_folder()[3] + '-' + str(TODAY) + ".bak'"
+Request4 = 'exec SP_DBREINDEX'
+# Request5 = "DBCC SHRINKDATABASE(N'" + SRW_688+")"
--- MaintenanceDB_tool/MaintenanceDB_tool.py
+++ MaintenanceDB_tool/MaintenanceDB_tool.py
@@ -3,12 +3,17 @@ from DB_tools_params import *
 from DB_tools_class import sql_return
 
 
+
+
 def connect_sql():
     cursor = sql_return().cursor()
     cursor.execute(Request3)
     time.sleep(1)
     if cursor.nextset() is True:
         print("Бэкап готов")
+        # cursor.execute(Request5)
+        # for row in cursor:
+        #     print(row)
     else:
         print("Неудача")
     sql_return().close()
