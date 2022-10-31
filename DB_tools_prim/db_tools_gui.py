8aa50003b1a6369c5e34bbe121b20dd823e2744c
redreoublics <redrepublics@yandex.ru>
2022-10-31 16:04:45 +0300

    merge origin/master #debugging db_tools

--- DB_tools_prim/db_tools_gui.py
+++ DB_tools_prim/db_tools_gui.py
@@ -1,23 +1,34 @@
 import os
+import sys
 
 from tkinter import *
+import tkinter.ttk as ttk
+
+
 x = os.getcwd()
 y = 'FontAwesome_f085(0).ico'
 window = Tk()
 window.iconbitmap(os.path.join(x, y))
-window.title("Обслуживание баз АСПО")
-window.geometry('550x200')
+window.title("DB Tools v.1.0.0.1")
+window.geometry('350x250')
 
 txtarea = Text(window, width=45, height=10)
-txtarea.place(x=250, y=95, anchor="c")
+# txtarea.place(x=150, y=120, anchor="c")
 txtarea.bind("<Key>", lambda e: "break")
+txtarea.place(relx=0.5, rely=0.5, anchor="c", relwidth=0.7, relheight=0.7)
 
 
 def run():
-    os.system('python db_tools.py')
+    os.system('python start_db.py')
 
 
-btn = Button(window,  text="START",  command=run)
-btn.grid(column=0, row=0)
+def run2():
+    sys.exit(1)
+
+
+ttk.Button(window, text="Старт", command=run).place(relx=0.3, rely=0.01)
+ttk.Button(window, text="Выход", command=run2).place(relx=0.5, rely=0.01)
+# btn.grid(column=0, row=0)
+# btn2.grid(column=15, row=0)
 
 window.mainloop()
---
+++ DB_tools_prim/start_db.py
@@ -0,0 +1,2 @@
+from db_tools import bak_sql
+bak_sql()
