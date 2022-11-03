0938c2b36cf57ef4f6eebde6ab7ab935a2d84fef
vjerebiov <redrepublics@yandex.ru>
2022-11-03 16:29:44 +0300

    merge origin/master #PEP-8

--- ping_test/ping_point.py
+++ ping_test/ping_point.py
@@ -3,7 +3,7 @@ import pyodbc
 from ping_params import *
 
 # Подключаемся к серверу
-hostname = []
+hostname = ['192.1.1.0']
 conn = pyodbc.connect(
     'DRIVER={' + get_folder()[0] + '};SERVER=' + get_folder()[1] + ';DATABASE=' + get_folder()[2] + ';UID=' +
     get_folder()[3] + ';PWD=' + get_folder()[4] + ';', autocommit=True)
@@ -15,27 +15,23 @@ for row in cursor:
     x = ",".join(row)
     hostname.append(x)
 
-
 # перебираем список, в случае успеха в один файл, в случае провала в другой, попутно выводим в консоль, что и как
 def ping_point():
     for i in hostname:
-        i = str(i)
-        if int(get_folder()[5]) == output_t:
-            print(f'{i}')
-        else:
-            pass
+        i: str = i
         response = os.system(f'ping {i} > nul')
         if response == 0:
-            if int(get_folder()[5]) == output_t:
-                print('Успех')
-            else:
-                pass
+            print(f'{i}')
+            print('Успех')
             with open("ping_success.txt", 'w+') as file:
                 file.write(f'{current_time} Доступен: {i}\n')
-        else:
+        elif response != 0:
+            print(f'{i}')
             print('Провал')
             with open('ping_failure.txt', 'w+') as file:
                 file.write(f'{current_time} Не доступен: {i}\n')
+        else:
+            pass
     cursor.close()
 
 
