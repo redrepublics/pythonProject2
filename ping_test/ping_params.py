9b83cb3294a02ee436a4764f0bc21dec831ada6d
redreoublics <redrepublics@yandex.ru>
2022-11-03 16:06:37 +0300

    merge origin/master #чистим логи по всей сущности

--- ping_test/ping_params.py
+++ ping_test/ping_params.py
@@ -1,18 +1,22 @@
 import configparser
+import datetime
 
-
+# тут забираем параметры из ini и выводим их в список
 ini_files = 'ping.ini'
 config = configparser.ConfigParser()
 config.read(ini_files)
+# отлавливаем временную метку для записи в лог
+now = datetime.datetime.now()
+current_time = now.strftime("%y_%m_%d_%H_%M_%S")
+output_t = 1
 
 
 def get_folder():
     ini_list = list()
     ini_list.append(config["connectdb"]['driver'])
     ini_list.append(config["connectdb"]['server'])
-    ini_list.append(config["connectdb"]['bddata'])
+    ini_list.append(config["connectdb"]['bd_data'])
     ini_list.append(config["connectdb"]['user'])
     ini_list.append(config["connectdb"]['password'])
+    ini_list.append(config["terminal"]['terminal_output'])
     return ini_list
-
-print(get_folder())
---
+++ ping_test/ping_point.py
@@ -0,0 +1,45 @@
+import os
+import pyodbc
+from ping_params import *
+
+# Подключаемся к серверу
+hostname = []
+conn = pyodbc.connect(
+    'DRIVER={' + get_folder()[0] + '};SERVER=' + get_folder()[1] + ';DATABASE=' + get_folder()[2] + ';UID=' +
+    get_folder()[3] + ';PWD=' + get_folder()[4] + ';', autocommit=True)
+cursor = conn.cursor()
+# делаем запрос на IP
+cursor.execute('select IPAddress from tNodesDetails')
+# причесываем IP и загоняем в список
+for row in cursor:
+    x = ",".join(row)
+    hostname.append(x)
+
+
+# перебираем список, в случае успеха в один файл, в случае провала в другой, попутно выводим в консоль, что и как
+def ping_point():
+    for i in hostname:
+        i = str(i)
+        if int(get_folder()[5]) == output_t:
+            print(f'{i}')
+        else:
+            pass
+        response = os.system(f'ping {i} > nul')
+        if response == 0:
+            if int(get_folder()[5]) == output_t:
+                print('Успех')
+            else:
+                pass
+            with open("ping_success.txt", 'w+') as file:
+                file.write(f'{current_time} Доступен: {i}')
+        else:
+            if int(get_folder()[5]) == output_t:
+                print('Провал')
+            else:
+                pass
+            with open('ping_failure.txt', 'w+') as file:
+                file.write(f'{current_time} Не доступен: {i}')
+    cursor.close()
+
+
+ping_point()
