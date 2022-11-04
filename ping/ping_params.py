a9bc014178b5553f22893036da0f1cf39be04293
redreoublics <redrepublics@yandex.ru>
2022-11-04 12:26:39 +0300

    merge origin/master #отлов живых поинтов

--- ping/ping_params.py
+++ ping/ping_params.py
@@ -1,5 +1,6 @@
 import configparser
 import datetime
+import os
 
 # тут забираем параметры из ini и выводим их в список
 ini_files = 'ping.ini'
@@ -8,9 +9,11 @@ config.read(ini_files)
 # отлавливаем временную метку для записи в лог
 now = datetime.datetime.now()
 current_time = now.strftime("%y.%m.%d %H:%M:%S")
+current_time_file = now.strftime("%y_%m_%d_%H_%M_%S")
 output_t = 1
-row = '\\'
-hostname = ['8.8.8.8', '192.168.1.1\sql19', 'ya.ru', 'localhost', '127.0.0.1', 'habr.com']
+del_t = '\\'
+# hostname = ['8.8.8.8', '192.168.1.1\sql19', 'ya.ru', 'localhost', '127.0.0.1', 'habr.com']
+hostname = []
 
 
 def get_folder():
@@ -22,3 +25,10 @@ def get_folder():
     ini_list.append(config["connectdb"]['password'])
     ini_list.append(config["terminal"]['terminal_output'])
     return ini_list
+
+# Создаем папку для хранения отчетов
+def folder_txt():
+    if not os.path.exists(os.path.join(os.getcwd(), 'отчеты')):
+        os.makedirs(os.path.join(os.getcwd(), 'отчеты'))
+    else:
+        pass
--- ping/ping_point.py
+++ ping/ping_point.py
@@ -1,51 +1,63 @@
 import pyodbc
 from ping_params import *
 from ping3 import ping
+import sys
+
 
 # Подключаемся к серверу
-conn = pyodbc.connect(
-    'DRIVER={' + get_folder()[0] + '};SERVER=' + get_folder()[1] + ';DATABASE=' + get_folder()[2] + ';UID=' +
-    get_folder()[3] + ';PWD=' + get_folder()[4] + ';', autocommit=True)
-cursor = conn.cursor()
-# делаем запрос на IP
-cursor.execute('select IPAddress from tNodesDetails')
-# причесываем IP и загоняем в список
-for row in cursor:
-    x = ",".join(row)
-    hostname.append(x)
-
-
-# выявляем в списке все что с экземплярами, и убираем, делаем по ним отчет
-for person in hostname:
-    if row in person:
-        hostname.remove(person)
-        with open("Сомнительная запись.txt", 'w+') as file:
-            file.write(f'{current_time} Проверить вручную: {person}\n')
+# делаем запрос на IP, создаем список, убираем все лишнее
+def host_name():
+    folder_txt()
+    try:
+        conn = pyodbc.connect(
+            "DRIVER={" + get_folder()[0] + '};SERVER=' + get_folder()[1] + ';DATABASE=' + get_folder()[2] + ';UID=' +
+            get_folder()[3] + ';PWD=' + get_folder()[4] + ';', autocommit=True)
+    except pyodbc.OperationalError as err:
+        err_report_one = 'Error: Ошибка подключения к SQL Server.'
+        err_report_two = ('ms-sql Operation Error: {0}'.format(err))
+        with open(os.path.join(os.getcwd(), 'отчеты', f'{current_time_file} Ошибка подключения.txt'), 'w+') as result:
+            result.write(r'' + err_report_one + '\n' + err_report_two + '')
+            result.close()
+            print(f"Выполнение будет остановлено. {err_report_one},\n{err_report_two}")
+            sys.exit(1)
     else:
-        pass
+        cursor = conn.cursor()
+    cursor.execute('select IPAddress from tNodesDetails')
+    for row in cursor:
+        x = ",".join(row)
+        hostname.append(x)
+        for person in hostname:
+            if del_t in person:
+                hostname.remove(person)
+                with open(os.path.join(os.getcwd(), 'отчеты', f"{current_time_file} Сомнительная запись.txt"),
+                          'w+') as file:
+                    file.write(f'{current_time} Проверить вручную: {person}\n')
+                    file.close()
+            else:
+                pass
+    cursor.close()
+    return hostname
 
 
 # смотрим ответ по выводу
-# надо проверить на английской версии OC
 def ping_point():
-    for i in hostname:
+    for i in host_name():
         i = str(i)
         ping(i)
         if ping(i):
-            with open("Доступные_хосты.txt", 'w+') as file:
+            with open(os.path.join(os.getcwd(), 'отчеты', f"{current_time_file} Доступные_хосты.txt"), 'a') as file:
                 file.write(f'{current_time} Доступен: {i}\n')
             if int(get_folder()[5]) == 1:
                 print(f'{i} - Успех.')
             else:
                 pass
         else:
-            with open('Недоступные_хосты.txt', 'w+') as file:
+            with open(os.path.join(os.getcwd(), 'отчеты', f"{current_time_file} Недоступные_хосты.txt"), 'a') as file:
                 file.write(f'{current_time} Недоступен: {i}\n')
             if int(get_folder()[5]) == 1:
                 print(f'{i} - Провал.')
             else:
                 pass
-    cursor.close()
 
 
 ping_point()
