29a70ec41f4d9ca991ca4acecc9a92f6200276c6
redreoublics <redrepublics@yandex.ru>
2022-11-07 02:30:35 +0300

    merge origin/master #ping point + чтение csv, обработка ошибок и отсутствия, count

--- PT/test/unin19_3_3.py
+++ PT/test/unin19_3_3.py
@@ -1,58 +1,41 @@
 import requests
 
-
-# headers = {'Accept': 'application/json', 'auth_key': 'a98adb702ee0d69b617573022e5077882aaf84aff129cb6376db68ef'}
-# base_url = 'https://petfriends.skillfactory.ru/'
-#
-# # get all pets
-# url = base_url + 'api/pets'
-# params = {'filter': 'my_pets'}
-#
-# resp_b = requests.get(url, params=params, headers=headers)
+# all params
 status = 'available'
-res_b = requests.get(f"https://petstore.swagger.io/v2/pet/findByStatus?status={status}",
-                   headers={'accept': "application/json"})
-print(res_b.text)
-print(res_b.json())
-for row in res_b.json():
-    # x = row['pets']['id']
-    print(row)
-    dict_pet = dict()
-    # print(x)
+base_url = 'https://petstore.swagger.io/v2/pet/'
+api_key_url = 'https://petfriends.skillfactory.ru/api/key'
+res_b = requests.get(f"{base_url}findByStatus?status={status}", headers={'accept': "application/json"})
+
+pet_dict = res_b.json()[0]
+pet_id = pet_dict['id']
+name = pet_dict['name']
+
+def api_res():
+    email = 'mailo4@mail.ru'
+    password = 'daorliar'
+    api_key_get = requests.get(f'{api_key_url}', headers={'email': f'{email}', 'password': f'{password}'})
+    api_list = api_key_get.json()["key"]
+    api_key = api_list
+    return api_key
 
 
-# # params request
-# pet_id = 12
-# name = 'doggie'
-# status = 'available'
-# res_pet_id = requests.get(f"https://petstore.swagger.io/v2/pet/findByStatus?status={status}",
-#                           headers={'accept': "application/json"})
-# # res_pet_id_text = dict(res_pet_id.text)
-# print(res_pet_id.text)
-# print(type(res_pet_id.text))
-# print(res_pet_id.text[0])
-#
-# # api_key = requests.post('https://petfriends.skillfactory.ru/api/key', headers={'email': 'mailo4@mail.ru', 'password': 'daorliar'})
-# # print(api_key.text)
-#
-#
-#
-#
-# # get request
-# res = requests.get(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers={'accept': 'application/json'})
-# if res.status_code == 200:
-#     print('Correct server response: ', res.status_code, '\n', res.json())
-# else:
-#     print('Incorrect server response: ', res.status_code, '\n', res.json())
-#
-#
-# # post request
-# res2 = requests.post(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers={'accept': 'application/json'},
-#                      data=f'name={name}&status={status}')
-# if res2.status_code == 200:
-#     print('Correct server response: ', res2.status_code, '\n', res2.json())
-# else:
-#     print('Incorrect server response: ', res2.status_code, '\n', res2.json())
-#
-# # delete request
-# # res3 = requests.delete()
+class TestClass:
+    # get request
+    def test_get(self):
+        res = requests.get(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers={'accept': 'application/json'})
+        if res.status_code == 200:
+            print('Correct server response for GET: ', res.status_code, '\n', res.json())
+        else:
+            print('Incorrect server response for GET: ', res.status_code, '\n', res.json())
+
+    # post request
+    def test_post(self):
+        res2 = requests.post(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers={'accept': 'application/json'},
+                             data=f'name={name}&status={status}')
+        if res2.status_code == 200:
+            print('Correct server response for POST: ', res2.status_code, '\n', res2.json())
+        else:
+            print('Incorrect server response Post: ', res2.status_code, '\n', res2.json())
+
+# delete request
+# res3 = requests.delete()
