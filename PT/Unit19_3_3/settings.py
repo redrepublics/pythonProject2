388bbc2b21a14e570914d54f5eead92d6da3c93d
redreoublics <redrepublics@yandex.ru>
2022-11-09 11:40:01 +0300

    merge origin/master

--- PT/Unit19_3_3/settings.py
+++ PT/Unit19_3_3/settings.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
-from api import PetFriends
 
 load_dotenv()
 valid_email = os.getenv('valid_email')
 valid_password = os.getenv('valid_password')
-pf = PetFriends()
+
--- PT/Unit19_3_3/tests/tests_unit19_3_3.py
+++ PT/Unit19_3_3/tests/tests_unit19_3_3.py
@@ -1,14 +1,16 @@
-"""tests unit 19.3.3
-Только положительные тесты."""
-from settings import *
+"""Tests unit 19.3.3
+Только положительные тесты.
+Импортируем все переменные"""
+from Unit19_3_3.settings import *
+from Unit19_3_3.api import PetFriends
+pf = PetFriends()
 
 
-def test_resp_api_key(email=valid_email, password=valid_password):
+def test_my_api_key(email=valid_email, password=valid_password):
     """Запрашиваем API_KEY подставив валидную электронную почту и пароль
     Запрос GET"""
     status, result = pf.get_api_key(email, password)
-    assert status == 200
-    assert 'key' in result
+    assert status == 200 and 'key' in result
 
 
 def test_get_pet(filter=''):
@@ -18,16 +20,14 @@ def test_get_pet(filter=''):
     Запрос GET"""
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.get_list_of_pets(auth_key, filter)
-
-    assert status == 200
-    assert len(result['pets']) > 0
+    assert status == 200 and len(result['pets']) > 0
 
 
 def test_put_pet(name='Мурзик', animal_type='Котэ', age=5):
     """Изменяем информацию о питомце.
     Сначала получаем ключ и список своих питомцев.
     Даем условие наличия питомца больше нуля,
-    иначе регаем ошибку.
+    иначе ловим ошибку.
     Запрос PUT"""
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
@@ -44,20 +44,16 @@ def test_del_pet():
     Если своих питомцев нет, добавляем материал для работы.
     Иначе работает с тем, что есть.
     Забираем первого питомца по идентификатору, отправляем запрос на удаление.
-    После финалим запрос, проверяем что идентификатор отсутствует.
+    После - запрос, проверяем что идентификатор отсутствует.
     Запрос DELETE"""
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
 
     if len(my_pets['pets']) == 0:
-        pf.add_new_pet(auth_key, "Суперкот", "кот", "3", "images/cat1.jpg")
+        pf.add_new_pet(auth_key, "Барсик", "кот", "3", "images/cat1.jpg")
         _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
 
     pet_id = my_pets['pets'][0]['id']
     status, _ = pf.delete_pet(auth_key, pet_id)
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
     assert status == 200 and pet_id not in my_pets.values()
-
-
-
-
