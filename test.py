72ddfa2f36a2edaeeae9bb751d7c589762c40792
redreoublics <redrepublics@yandex.ru>
2022-11-18 10:19:25 +0300

    merge origin/master

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-name = 1
-x = isinstance(name, str)
-print(type(x))
-print(x)
+
+def most_frequent(input_list):
+    counter = 0
+    num = input_list[0]
+
+    for i in input_list:
+        curr_frequency = input_list.count(i)
+        if curr_frequency > counter:
+            counter = curr_frequency
+            num = i
+
+    return num
+
+
+result = input("Введити слова через пробел, по окончанию нажмите Enter: ").split()
+print(most_frequent(result))
