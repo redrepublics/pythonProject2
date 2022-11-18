2e676452e9f30354fb24664761dfaad0c4aafcb9
redreoublics <redrepublics@yandex.ru>
2022-11-18 13:40:50 +0300

    merge origin/master

--- Unint21/decorator.py
+++ Unint21/decorator.py
@@ -1,16 +1,25 @@
+from datetime import datetime
+
+
 def my_decorator(func):
     def wrapper():
         print("Начало выполнения функции.")
-        func()
+        value_hour = datetime.now().hour
+        if 9 <= datetime.now().hour < 18:
+            print('Выполняем функцию в рабочее время.')
+            print(f'Сейчас {value_hour} часов')
+            func()
+        else:
+            print('Время отдыха, функцию не выполняем.')
+            print(f'Сейчас {value_hour} часов')
         print("Конец выполнения функции.")
 
     return wrapper
 
 
+@my_decorator
 def my_first_decorator():
     print("Это мой первый декоратор!")
 
-
-my_first_decorator = my_decorator(my_first_decorator)
 
 my_first_decorator()
