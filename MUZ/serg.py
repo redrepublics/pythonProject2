367b9a14b8ece6b5e8f73d299e73e1f7ab6c3b7e
redreoublics <redrepublics@yandex.ru>
2022-12-02 10:35:50 +0300

    merge origin/master # забагованый калькулятор

--- MUZ/serg.py
+++ MUZ/serg.py
@@ -1,19 +1,19 @@
-import pygame
 from gtts import gTTS
 
 
-
+test_params = "- Скажите, пожалуйста, куда мне отсюда идти? " \
+              "Это во многом зависит от того, куда ты хочешь прийти, — ответил Кот." \
+              "Да мне почти всё равно, — начала Алиса." \
+              "Тогда всё равно, куда идти, — сказал Кот." \
+              "Лишь бы попасть куда-нибудь, — пояснила Алиса." \
+              "Не беспокойся, куда-нибудь ты обязательно попадешь, " \
+              "— сказал Кот, — конечно, если не остановишься на полпути." \
+              "Льюис Кэрролл, «Алиса в стране чудес»"
 
 audio = "audio.mp3"
 language = "ru"
-sp = gTTS(text="Сергей", lang=language, slow=False)
+sp = gTTS(text=test_params, lang=language, slow=False)
 sp.save(audio)
 
 
-pygame.init()
-song = pygame.mixer.Sound(audio)
-clock = pygame.time.Clock()
-song.play()
-while True:
-    clock.tick(60)
-pygame.quit()
+
