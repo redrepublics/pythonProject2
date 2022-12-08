d730a95c3b43899a7600a4bb5e799231d9880b7b
redreoublics <redrepublics@yandex.ru>
2022-12-08 13:39:48 +0300

    merge origin/master

---
+++ WhoisCALLME/call_v2.py
@@ -0,0 +1,35 @@
+import sys
+import requests
+
+
+class CallCLASS:
+    def __init__(self):
+        self.res_print = None
+        try:
+            self.tel_number = int(input('Введите номер телефона (только цифры: пример - 7921*******): '))
+        except ValueError:
+            print('Некорректный формат номера телефона.')
+        else:
+            pass
+        self.api_key = '1VBGxnPGmXuApLfYlFGta0L17JQnN63u50xVAuYA'
+        self.base_url = 'https://api.numlookupapi.com/v1/validate/'
+        try:
+            self.request = requests.get(f'{self.base_url}+{self.tel_number}?apikey={self.api_key}')
+        except AttributeError:
+            print('Ошибка в запросе API. Возможно вы некорректно ввели номер телефона.')
+            sys.exit(0)
+
+    def information_by_number(self):
+        self.res_print = self.request.json()
+        if False != self.res_print['valid']:
+            print('Нахождение номера: ', (self.res_print['location']))
+            print('Страна: ', self.res_print['country_name'])
+            if self.res_print['carrier']:
+                print('Оператор: ', self.res_print['carrier'])
+            print('Тип телефона: ', self.res_print['line_type'])
+        else:
+            print('По данному номеру телефона получить информацию нельзя.')
+
+
+result = CallCLASS()
+result.information_by_number()
