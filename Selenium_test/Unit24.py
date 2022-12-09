483ad5fa3fd0eb5dd6473062cf7e438aad5ce8c9
redreoublics <redrepublics@yandex.ru>
2022-12-09 10:50:22 +0300

    merge origin/master #selenium ui

--- Selenium_test/Unit24.py
+++ Selenium_test/Unit24.py
@@ -1,7 +1,17 @@
-import time
+# import time
+#
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+# driver.get('https://google.com')
+# driver.quit()
 
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('https://google.com')
-driver.quit()
+from pywinauto.application import Application
+# Запускаем целевое приложение - обычный блокнот
+app = Application().start("notepad.exe")
+# Выбираем пункт меню
+app.UntitledNotepad.menu_select("Справка->О программе")
+# Симулируем клик
+app.AboutNotepad.OK.click()
+# Вводим текст
+app.UntitledNotepad.Edit.type_keys("Заработало!", with_spaces = True)
