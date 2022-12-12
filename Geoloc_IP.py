b5daa1e46794bf74c9e7784e1c3164c676a649ae
redreoublics <redrepublics@yandex.ru>
2022-12-12 20:37:38 +0300

    merge origin/master #selenium ui

--- Geoloc_IP.py
+++ Geoloc_IP.py
@@ -1,6 +1,16 @@
 import requests
 
-ip = requests.get('http://ipinfo.io')
-ip_res = ip.json()
-print(ip_res['loc'])
 
+class IpRES:
+    def __init__(self):
+        self.ip = requests.get('http://ipinfo.io')
+        self.ip_res = self.ip.json()
+
+    def result_ip(self):
+        print('IP: {0}\nГород: {1}\nСтрана: {3}\nКоординаты провайдера: {2}\nНаименование провайдера: {4}'
+              .format((self.ip_res['ip']), (self.ip_res['city']), (self.ip_res['loc']), (self.ip_res['country']),
+                      (self.ip_res['org'])))
+
+
+pr = IpRES()
+pr.result_ip()
