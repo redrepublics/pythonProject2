73f4f8b81bc03c0791d084fd4f931fd76702df38
redreoublics <redrepublics@yandex.ru>
2022-12-13 14:08:00 +0300

    merge origin/master #sock

--- Scanport/scanport.py
+++ Scanport/scanport.py
@@ -1,5 +1,9 @@
 import socket
 import datetime
+import os
+from scanport_params import report_file, ports
+
+"""Основная процедура, проверяет и пишет"""
 
 
 def scan_port(ip_add, port):
@@ -8,10 +12,10 @@ def scan_port(ip_add, port):
     now = datetime.datetime.now()
     current_time = now.strftime("%Y %b %d %A %H:%M:%S ")
     try:
-        with open('report.txt', 'a+') as file:
-            connect = sock.connect((ip_add, port))
-            print(current_time, ('-'*10), 'IP :', ip_add, 'Port :', port, ' its open', 'Protocol :',
-                  ports[port], ('-'*10))
+        with open(report_file, 'a+') as file:
+            sock.connect((ip_add, port))
+            print(current_time, ('-' * 10), 'IP :', ip_add, 'Port :', port, ' its open', 'Protocol :',
+                  ports[port], ('-' * 10))
             file.write('{0} IP: {1}, Port: {2}, Protocol: {3}\n'.format(current_time, ip_add, port, ports[port]))
             sock.close()
     except:
@@ -19,23 +23,30 @@ def scan_port(ip_add, port):
         pass
 
 
-ports = {
-    20: "FTP-DATA", 21: "FTP", 22: "SSH", 23: "Telnet",
-    25: "SMTP", 43: "WHOIS", 53: "DNS", 80: "http",
-    115: "SFTP", 123: "NTP", 143: "IMAP", 161: "SNMP",
-    179: "BGP", 443: "HTTPS", 445: "MICROSOFT-DS",
-    514: "SYSLOG", 515: "PRINTER", 993: "IMAPS",
-    995: "POP3S", 1080: "SOCKS", 1194: "OpenVPN",
-    1433: "SQL Server", 1723: "PPTP", 3128: "HTTP",
-    3268: "LDAP", 3306: "MySQL", 3389: "RDP",
-    5432: "PostgreSQL", 5900: "VNC", 8080: "Tomcat", 10000: "Webmin"}
+"""Контроль лога. Если пустой, то удаляем. Оставляем только с результатом."""
 
-ip_add = input('Введите IP хоста для сканирования: ')
-# '192.168.31.250'
-if ip_add:
-    for i in ports:
-        scan_port(ip_add, i)
-else:
-    ip_add = 'localhost'
-    for i in ports:
-        scan_port(ip_add, i)
+
+def null_files():
+    is_empty = os.stat(report_file).st_size == 0
+    if is_empty is True:
+        os.remove(report_file)
+    else:
+        pass
+
+
+"""Получаем данные хоста, перебираем словарь. В случае если ничего не ввели подменяем значение на localhost"""
+
+
+def run_poc():
+    ip_add = input('Введите IP хоста для сканирования: ')
+    if ip_add:
+        for i in ports:
+            scan_port(ip_add, i)
+    else:
+        ip_add = 'localhost'
+        for i in ports:
+            scan_port(ip_add, i)
+    null_files()
+
+
+run_poc()
