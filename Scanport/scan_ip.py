1bee77003c360e0db66d141b2936c203d08c17ba
redreoublics <redrepublics@yandex.ru>
2022-12-14 12:04:16 +0300

    Update Readme.md

--- Scanport/scan_ip.py
+++ Scanport/scan_ip.py
@@ -5,6 +5,7 @@ def my_ip():
     my_dict = (psutil.net_if_addrs())
     for key in my_dict:
         if key.startswith("Ethernet"):
-            print("found {} : {}".format(key, my_dict[key][1][1]))
+            return my_dict[key][1][1], my_dict[key][1][2], my_dict[key][0][1]
 
 
+print('IP {}, mask {}, mac {}'.format(my_ip()[0], my_ip()[1], my_ip()[2]))
