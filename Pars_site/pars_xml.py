703f2901aaaede997460f59b80ab4ffc5be3d9ca
redreoublics <redrepublics@yandex.ru>
2022-12-15 13:04:59 +0300

    Merge remote-tracking branch 'origin/master'

--- Pars_site/pars_xml.py
+++ Pars_site/pars_xml.py
@@ -2,13 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-link = 'https://browser-info.ru/'
+link = 'https://ya.ru/'
 responce = requests.get(link).text
 # with open('1.html', 'w', encoding='utf-8') as file:
 #     file.write(responce)
 #     file.close()
 soup = BeautifulSoup(responce, 'lxml')
-block = soup.find('div', id='tool_padding')
-check_js = block.find('div', id='javascript_check')
-result_js = check_js.find_all('span')[1].text
-print('JS {}'.format(result_js))
+block = soup.find('div', class="informers3")
+print()
