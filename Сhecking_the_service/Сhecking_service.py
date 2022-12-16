4774c75514f714b0fc1a74ab439ffabe5beb2b9e
redreoublics <redrepublics@yandex.ru>
2022-12-16 15:24:01 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ Сhecking_the_service/Сhecking_service.py
@@ -0,0 +1,44 @@
+import time
+
+import psutil
+
+""" Проверяем запущена ли служба, выводим по ней информацию.
+ Имя службы хранится в self.service_name."""
+
+
+class ChSERVICE:
+    def __init__(self):
+        self.res_start_type = 'start_type'
+        self.res_bin_path = 'binpath'
+        self.service = None
+        self.service_name = 'MSSQL$SQLEXPRESS'
+        self.d_name = 'display_name'
+
+    def checking_service(self):
+        count = 0
+        while True:
+            try:
+                self.service = psutil.win_service_get(self.service_name)
+                self.service = self.service.as_dict()
+            except Exception as err:
+                print(str(err))
+                return
+            if self.service:
+            # print(self.service)
+                print("Служба {2} найдена.\nМестоположение службы: {0}.\nТип запуска: {1}".
+                      format(self.service[self.res_bin_path], self.service[self.res_start_type], self.service[self.d_name]))
+                count += 1
+                time.sleep(0)
+                print(('-'*10), 'Опрос №', count-1, '\n')
+                if self.service and self.service['status'] == 'running':
+                    print('Служба запущена.')
+                elif count == 11:
+                    break
+                else:
+                    print("Служба не запущена.")
+            else:
+                print("Такой службы нет.")
+
+
+
+ChSERVICE().checking_service()
