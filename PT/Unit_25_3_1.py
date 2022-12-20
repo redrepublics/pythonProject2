ffe924da0454189362af27d7d67089882cfdbd11
redreoublics <redrepublics@yandex.ru>
2022-12-20 12:59:03 +0300

    Merge remote-tracking branch 'origin/master'

--- PT/Unit_25_3_1.py
+++ PT/Unit_25_3_1.py
@@ -11,13 +11,15 @@ url_test = 'http://petfriends.skillfactory.ru/login'
 url_my_pets = 'https://petfriends.skillfactory.ru/my_pets'
 
 
-
 @pytest.fixture(autouse=True)
 def testing():
     # Переходим на страницу авторизации
     pytest_driver.get(url_test)
     # yield
     # pytest_driver.quit()
+
+
+""" Тест на каунт питомцев и канут таблицы."""
 
 
 def test_show_my_pets():
@@ -47,6 +49,9 @@ def test_show_my_pets():
               .format(result_my_pets_count, result_my_pets_table))
 
 
+""" Тест на фото. """
+
+
 def test_img():
     pytest_driver.find_element(By.ID, 'email').send_keys(e_mail)
     # Вводим пароль
@@ -69,6 +74,9 @@ def test_img():
             result_img_pr = (100 / (len(my_pets_img) / count))
             print('\nБез фото всего {} %'.format(result_img_pr))
             assert result_img_pr == 50
+
+
+"""Тест на отсутствие или наличие какого либо параметра в списке."""
 
 
 def test_array():
@@ -116,3 +124,93 @@ def test_array():
     else:
         print('\nУ некоторых питомцев не указан возраст.')
     assert arr3_count == arr3_list and arr2_list == arr2_count and arr_list == arr_count
+
+
+""" Тест на уникальные имена. """
+
+
+def test_name():
+    pytest_driver.find_element(By.ID, 'email').send_keys(e_mail)
+    # Вводим пароль
+    pytest_driver.find_element(By.ID, 'pass').send_keys(pass_test)
+    # Нажимаем на кнопку входа в аккаунт
+    pytest_driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    time.sleep(1)
+    pytest_driver.find_element(By.CLASS_NAME, 'navbar-toggler-icon').click()
+    time.sleep(1)
+    pytest_driver.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a').click()
+    time.sleep(1)
+    name_pets = pytest_driver.find_elements(By.XPATH, '//*[@id="all_my_pets"]/table/tbody/tr/td[1]')
+    pet_name = []
+    for pet in name_pets:
+        pet_name.append(pet.text)
+    unique = [x for i, x in enumerate(pet_name) if i == pet_name.index(x)]
+    if len(pet_name) == len(unique):
+        print('\nВсе имена уникальны')
+    else:
+        print('\nЕсть повторяющиеся имена')
+    assert len(pet_name) == len(unique)
+
+
+""" Тест на уникальные записи. Если есть хоть одна не уникальная, будет провал."""
+
+
+def test_dub():
+    pytest_driver.find_element(By.ID, 'email').send_keys(e_mail)
+    # Вводим пароль
+    pytest_driver.find_element(By.ID, 'pass').send_keys(pass_test)
+    # Нажимаем на кнопку входа в аккаунт
+    pytest_driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    time.sleep(1)
+    pytest_driver.find_element(By.CLASS_NAME, 'navbar-toggler-icon').click()
+    time.sleep(1)
+    pytest_driver.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a').click()
+    time.sleep(1)
+    name_pets = pytest_driver.find_elements(By.XPATH, '//*[@id="all_my_pets"]/table/tbody/tr/td[1]')
+    breed_pets = pytest_driver.find_elements(By.XPATH, '//*[@id="all_my_pets"]/table/tbody/tr/td[2]')
+    age_pets = pytest_driver.find_elements(By.XPATH, '//*[@id="all_my_pets"]/table/tbody/tr/td[3]')
+    pet_name = []
+    pet_breed = []
+    pet_age = []
+    n = False
+    num_true = []
+    for pet in name_pets:
+        pet_name.append(pet.text)
+        unique = [x for i, x in enumerate(pet_name) if i == pet_name.index(x)]
+        if len(pet_name) == len(unique):
+            n = True
+            num_true.append(n)
+        else:
+            n = False
+            num_true.append(n)
+    for pet2 in breed_pets:
+        pet_breed.append(pet2.text)
+        unique2 = [x for i, x in enumerate(pet_breed) if i == pet_breed.index(x)]
+        if len(pet_breed) == len(unique2):
+            n = True
+            num_true.append(n)
+        else:
+            n = False
+            num_true.append(n)
+    for pet3 in age_pets:
+        pet_age.append(pet3.text)
+        unique3 = [x for i, x in enumerate(pet_age) if i == pet_age.index(x)]
+        if len(pet_age) == len(unique3):
+            n = True
+            num_true.append(n)
+        else:
+            n = False
+            num_true.append(n)
+    no_n = False
+    if no_n is num_true:
+        result = False
+    else:
+        result = True
+    if result is True:
+        print('\nВсе записи уникальны.')
+    else:
+        print('\nЗаписи не уникальны.')
+
+    assert result is True
