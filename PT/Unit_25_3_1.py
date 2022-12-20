c787e69684008ea7755c646e05ce839340cd6794
redreoublics <redrepublics@yandex.ru>
2022-12-20 12:59:03 +0300

    Merge remote-tracking branch 'origin/master'

--- PT/Unit_25_3_1.py
+++ PT/Unit_25_3_1.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 e_mail = 'test_user_1@mail.ru'
+user_my = 'test_user_1'
 pass_test = 'daorliar'
 pytest_driver = webdriver.Chrome('chromedriver.exe')
 url_test = 'http://petfriends.skillfactory.ru/login'
+url_my_pets = 'https://petfriends.skillfactory.ru/my_pets'
 
 
 @pytest.fixture(autouse=True)
@@ -26,18 +28,8 @@ def test_show_my_pets():
     pytest_driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
     # Проверяем, что мы оказались на главной странице пользователя
     time.sleep(1)
-    assert pytest_driver.find_element(By.TAG_NAME, 'h1').text == 'PetFriends'
-
-
-def test_m():
-    images = pytest_driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
-    names = pytest_driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
-    descriptions = pytest_driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
-    for i in range(len(names)):
-        assert images[i].get_attribute('src') != ''
-        assert names[i].text != ''
-        assert descriptions[i].text != ''
-        assert ', ' in descriptions[i]
-        parts = descriptions[i].text.split(", ")
-        assert len(parts[0]) > 0
-        assert len(parts[1]) > 0
+    pytest_driver.find_element(By.CLASS_NAME, 'navbar-toggler-icon').click()
+    time.sleep(1)
+    pytest_driver.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a').click()
+    time.sleep(1)
+    assert pytest_driver.find_element(By.TAG_NAME, 'h2').text == user_my
